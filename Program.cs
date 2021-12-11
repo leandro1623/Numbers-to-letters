2c83c3be3539ea4e3b8ca34ce28d2556b387e661
Leandro <leandrobritocomtreras17@gmail.com>
2021-12-11 12:22:33 -0400

    Numeros a letras primera vercion 1.0

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;                                 //Cifra mas alta traducida 999,999,999,999
+
+namespace Transformador
+{
+    class Program
+    {
+        static void Main()//-metodo main----------------------------------------
+        {
+            TransformNumbersToLetters.Tranform_And_Show_In_The_Console_The_Result();
+        }//-------------fin main-------------------------------------------------
+    }
+}
